7b437606358b7177be84f80c6aa06ccee0126193
r4coder <ritheeshk2003@gmail.com>
2026-09-21 19:20:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("btn");
 const input = document.getElementById("ip");
 const output = document.getElementById("output");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", function () {
     const value = Number(input.value);
 
     new Promise((resolve) => {
@@ -11,37 +11,39 @@ btn.addEventListener("click", () => {
         }, 2000);
     })
     .then((result) => {
-        const newResult = result * 2;
-        output.textContent = "Result: " + newResult;
-
         return new Promise((resolve) => {
             setTimeout(() => {
+                const newResult = result * 2;
+                output.textContent = "Result: " + newResult;
                 resolve(newResult);
             }, 2000);
         });
     })
     .then((result) => {
-        const newResult = result - 3;
-        output.textContent = "Result: " + newResult;
-
         return new Promise((resolve) => {
             setTimeout(() => {
+                const newResult = result - 3;
+                output.textContent = "Result: " + newResult;
                 resolve(newResult);
             }, 2000);
         });
     })
     .then((result) => {
-        const newResult = result / 2;
-        output.textContent = "Result: " + newResult;
-
         return new Promise((resolve) => {
             setTimeout(() => {
+                const newResult = result / 2;
+                output.textContent = "Result: " + newResult;
                 resolve(newResult);
             }, 2000);
         });
     })
     .then((result) => {
-        const newResult = result + 10;
-        output.textContent = "Final Result: " + newResult;
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const newResult = result + 10;
+                output.textContent = "Final Result: " + newResult;
+                resolve(newResult);
+            }, 2000);
+        });
     });
 });
